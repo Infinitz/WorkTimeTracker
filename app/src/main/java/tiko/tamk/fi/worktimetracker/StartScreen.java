eb39e7ace84b3ebdf8a6fe0e163025fd41586bbc
Infinitz <ristopulkkinen94@gmail.com>
2018-03-09 07:21:14 +0200

    Add functionality to add work time button to open SaveWorkTimeScreen

--- app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
@@ -1,7 +1,9 @@
 package tiko.tamk.fi.worktimetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+    }
+
+    public void addWorkTime(View view) {
+        Intent intent = new Intent(this, SaveWorkTimeScreen.class);
+        startActivity(intent);
     }
 }
