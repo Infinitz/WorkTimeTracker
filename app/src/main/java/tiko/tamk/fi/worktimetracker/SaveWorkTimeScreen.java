480551cf11f6d4db6530cb850c16722d0c1ced91
Infinitz <ristopulkkinen94@gmail.com>
2018-03-09 07:33:48 +0200

    Add EditText field in SaveWorkTimeScreen for saving working hours

--- app/src/main/java/tiko/tamk/fi/worktimetracker/SaveWorkTimeScreen.java
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/SaveWorkTimeScreen.java
@@ -2,6 +2,7 @@ package tiko.tamk.fi.worktimetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SaveWorkTimeScreen extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class SaveWorkTimeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_work_time_screen);
+
+        EditText workHourEdit = (EditText) findViewById(R.id.workHourEdit);
     }
 }
