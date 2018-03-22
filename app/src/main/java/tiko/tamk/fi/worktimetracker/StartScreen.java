3e1465b7093a931560d7668aadd00fe474762a32
Infinitz <ristopulkkinen94@gmail.com>
2018-03-22 11:22:53 +0200

    Modify ActionBar button methods in Startscreen to have some test functionality

--- app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -32,11 +34,19 @@ public class StartScreen extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case (R.id.menuAddButton):
+                Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
     }
 }
