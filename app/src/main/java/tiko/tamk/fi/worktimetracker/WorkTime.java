76af5e9aa6c9fa3e3dd4687be17e9e3bb1f2ca61
Infinitz <ristopulkkinen94@gmail.com>
2018-03-22 10:56:34 +0200

    Add WorkTime class for saving working times.

---
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/WorkTime.java
@@ -0,0 +1,41 @@
+package tiko.tamk.fi.worktimetracker;
+
+/**
+ * Created by risto on 22.3.2018.
+ */
+
+public class WorkTime {
+    private String title;
+    private double hours;
+    private String description;
+
+    public WorkTime(String title, double hours, String description) {
+        this.title = title;
+        this.hours = hours;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
