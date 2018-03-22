7c5b3ffa08f5009a3742814842ad418453d8da7c
Infinitz <ristopulkkinen94@gmail.com>
2018-03-22 11:19:35 +0200

    Add mymenu xml file to control Action Bar menu button Add required methods to StartScreen

--- app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
@@ -3,6 +3,8 @@ package tiko.tamk.fi.worktimetracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,5 +28,15 @@ public class StartScreen extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, items);
 
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
