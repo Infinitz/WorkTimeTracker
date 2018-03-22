44086878ca1b527f1a24988c0a46b1e870d8c30e
Infinitz <ristopulkkinen94@gmail.com>
2018-03-22 11:13:17 +0200

    Delete activities other than StartScreen Modify startScreen to contain the listView

--- app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
+++ app/src/main/java/tiko/tamk/fi/worktimetracker/StartScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -11,15 +13,18 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-    }
 
-    public void addWorkTime(View view) {
-        Intent intent = new Intent(this, SaveWorkTimeScreen.class);
-        startActivity(intent);
-    }
+        ListView listView = (ListView) findViewById(R.id.listView);
 
-    public void browseWorkTime(View view) {
-        Intent intent = new Intent(this, BrowseWorkTimesScreen.class);
-        startActivity(intent);
+        WorkTime[] items = {
+                new WorkTime("Title1", 8, "Description1"),
+                new WorkTime("Title2", 8, "Description2"),
+                new WorkTime("Title3", 8, "Description3"),
+        };
+
+        ArrayAdapter<WorkTime> adapter = new ArrayAdapter<WorkTime>(this,
+                android.R.layout.simple_list_item_1, items);
+
+        listView.setAdapter(adapter);
     }
 }
